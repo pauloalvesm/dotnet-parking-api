ca2308c5e10f3f6c7effe617926158e8b133720f
pauloalvesm <paulo.alves7351@gmail.com>
2026-08-09 21:01:06 -0300

    feat: add validation class

---
+++ src/Parking.Domain/Validations/DomainExceptionValidation.cs
@@ -0,0 +1,16 @@
+﻿namespace Parking.Domain.Validations;
+
+public class DomainExceptionValidation : Exception
+{
+    public DomainExceptionValidation() { }
+
+    public DomainExceptionValidation(string error) : base(error) { }
+
+    public static void GetErrors(bool hasErros, string error)
+    {
+        if (hasErros)
+        {
+            throw new DomainExceptionValidation(error);
+        }
+    }
+}
