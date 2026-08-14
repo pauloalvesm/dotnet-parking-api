6ce7052f5814643a53fa78c0ae2ba4a7d743aa20
pauloalvesm <paulo.alves7351@gmail.com>
2026-08-13 21:15:25 -0300

    feat: add entity settings

---
+++ src/Parking.Data/EntitiesConfiguration/CustomerVehicleConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Parking.Domain.Entities;
+
+namespace Parking.Data.EntitiesConfiguration;
+
+public class CustomerVehicleConfiguration : IEntityTypeConfiguration<CustomerVehicle>
+{
+    public void Configure(EntityTypeBuilder<CustomerVehicle> builder)
+    {
+        builder.ToTable("CustomerVehicles");
+
+        builder.HasKey(cv => cv.Id);
+
+        builder.HasOne(cv => cv.Customer)
+            .WithMany(c => c.CustomerVehicles)
+            .HasForeignKey(cv => cv.CustomerId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(cv => cv.Vehicle)
+            .WithMany(v => v.CustomerVehicles)
+            .HasForeignKey(cv => cv.VehicleId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
