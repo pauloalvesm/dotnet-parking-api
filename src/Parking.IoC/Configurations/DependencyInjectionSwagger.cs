0a094ee3cbc0842bc52dd345d1b0afdd4ca9a9c7
pauloalvesm <paulo.alves7351@gmail.com>
2026-08-21 09:47:05 -0300

    feat: apply the configuration for using Swagger

---
+++ src/Parking.IoC/Configurations/DependencyInjectionSwagger.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi;
+
+namespace Parking.IoC.Configurations;
+
+public static class DependencyInjectionSwagger
+{
+    public static IServiceCollection AddInfrastructureSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Parking",
+                Version = "v1",
+                Description = "API to simulate a parking management system.",
+                Contact = new OpenApiContact
+                {
+                    Name = "Paulo Alves",
+                    Email = "paulo.alves7351@gmail.com",
+                    Url = new Uri("https://github.com/pauloalvesm"),
+                },
+            });
+        });
+
+        return services;
+    }
+}
