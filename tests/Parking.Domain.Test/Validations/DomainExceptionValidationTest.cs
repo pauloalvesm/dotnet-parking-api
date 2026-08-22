6ce13cd3f4ab708aa856ccb1c5290a46d3326f2f
pauloalvesm <paulo.alves7351@gmail.com>
2026-08-22 15:42:27 -0300

    feat: implement tests to validate the domains

---
+++ tests/Parking.Domain.Test/Validations/DomainExceptionValidationTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Parking.Domain.Validations;
+
+namespace Parking.Domain.Test.Validations;
+
+public class DomainExceptionValidationTest
+{
+    [Fact(DisplayName = "GetErrors - No Errors No Exception Thrown")]
+    public void DomainExceptionValidation_GetErrors_NoErrorsNoExceptionThrown()
+    {
+
+        Action action = () => DomainExceptionValidation.GetErrors(false, "An error occurred.");
+
+        action.Should().NotThrow();
+    }
+
+    [Fact(DisplayName = "GetErrors - Errors Domain Exception Thrown")]
+    public void DomainExceptionValidation_GetErrors_ErrorsDomainExceptionThrown()
+    {
+
+        Action action = () => DomainExceptionValidation.GetErrors(true, "An error occurred.");
+
+        action.Should().Throw<DomainExceptionValidation>().WithMessage("An error occurred.");
+    }
+}
