d5c804c9b2fcced0cfe7988b68527fd14e6a4b4a
pauloalvesm <paulo.alves7351@gmail.com>
2026-09-06 13:25:14 -0300

    feat: apply tests for the AddressService

---
+++ tests/Parking.Service.Test/Helpers/AddressTestHelper.cs
@@ -0,0 +1,47 @@
+﻿using Parking.Domain.Entities;
+using Parking.Service.DTOs;
+
+namespace Parking.Service.Test.Helpers;
+
+public static class AddressTestHelper
+{
+    public static AddressDTO CreateValidAddressDTO(int id = 1)
+    {
+        return new AddressDTO
+        {
+            Id = id,
+            Street = "Main Street",
+            Number = "123",
+            Complement = "Suite 101",
+            Neighborhood = "Downtown",
+            FederativeUnit = "SP",
+            City = "São Paulo",
+            ZipCode = "12345-678"
+        };
+    }
+
+    public static Address CreateValidAddressEntity(int id = 1)
+    {
+        var address = (Address)System.Runtime.CompilerServices.RuntimeHelpers.GetUninitializedObject(typeof(Address));
+
+        SetPropertyIfExists(address, nameof(Address.Id), id);
+        SetPropertyIfExists(address, "Street", "Main Street");
+        SetPropertyIfExists(address, "Number", "123");
+        SetPropertyIfExists(address, "Complement", "Suite 101");
+        SetPropertyIfExists(address, "Neighborhood", "Downtown");
+        SetPropertyIfExists(address, "FederativeUnit", "SP");
+        SetPropertyIfExists(address, "City", "São Paulo");
+        SetPropertyIfExists(address, "ZipCode", "12345-678");
+
+        return address;
+    }
+
+    private static void SetPropertyIfExists(object obj, string propertyName, object value)
+    {
+        var prop = obj.GetType().GetProperty(propertyName);
+        if (prop != null && prop.CanWrite)
+        {
+            prop.SetValue(obj, value);
+        }
+    }
+}
