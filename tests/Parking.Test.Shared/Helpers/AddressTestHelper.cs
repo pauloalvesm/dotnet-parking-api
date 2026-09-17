394c9d144b7cd4dd1606992ca6be5144b70b5a49
pauloalvesm <paulo.alves7351@gmail.com>
2026-09-17 20:53:33 -0300

    fix: refactor class help for Address

---
+++ tests/Parking.Test.Shared/Helpers/AddressTestHelper.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using Parking.Domain.Entities;
+using Parking.Service.DTOs;
+
+namespace Parking.Test.Shared.Helpers;
+
+public static class AddressTestHelper
+{
+    public static AddressDTO CreateValidAddressDTO(int id = 1)
+    {
+        return new AddressDTO
+        {
+            Id = id,
+            Street = "Main Street",
+            Number = "123",
+            Complement = "Suite 101",
+            Neighborhood = "Downtown",
+            FederativeUnit = "SP",
+            City = "São Paulo",
+            ZipCode = "12345-678"
+        };
+    }
+
+    public static Address CreateValidAddressEntity(int id = 1)
+    {
+        var address = (Address)System.Runtime.CompilerServices.RuntimeHelpers.GetUninitializedObject(typeof(Address));
+
+        SetPropertyIfExists(address, nameof(Address.Id), id);
+        SetPropertyIfExists(address, nameof(Address.Street), "Main Street");
+        SetPropertyIfExists(address, nameof(Address.Number), "123");
+        SetPropertyIfExists(address, nameof(Address.Complement), "Suite 101");
+        SetPropertyIfExists(address, nameof(Address.Neighborhood), "Downtown");
+        SetPropertyIfExists(address, nameof(Address.FederativeUnit), "SP");
+        SetPropertyIfExists(address, nameof(Address.City), "São Paulo");
+        SetPropertyIfExists(address, nameof(Address.ZipCode), "12345-678");
+
+        return address;
+    }
+
+    private static void SetPropertyIfExists(object obj, string propertyName, object value)
+    {
+        var prop = obj.GetType().GetProperty(propertyName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+        if (prop != null && prop.CanWrite)
+        {
+            prop.SetValue(obj, value);
+        }
+    }
+}
