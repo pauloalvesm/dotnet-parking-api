01e03bf86c6121511b41771b89612d12caf3bd6a
pauloalvesm <paulo.alves7351@gmail.com>
2026-09-24 11:23:19 -0300

    feat: create classes for login

---
+++ src/Parking.Service/DTOs/Account/LoginDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parking.Service.DTOs.Account;
+
+public class LoginDTO
+{
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    public string Password { get; set; }
+}
---
+++ src/Parking.Service/DTOs/Account/UserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parking.Service.DTOs.Account;
+
+public class UserDTO
+{
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+    public string Password { get; set; }
+
+    [Required(ErrorMessage = "Password confirmation is required.")]
+    [Compare("Password", ErrorMessage = "Passwords do not match.")]
+    public string ConfirmPassword { get; set; }
+}
